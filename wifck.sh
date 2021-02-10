d44984a553b41152b10fbc07dc7666279c145bf8
xG4L1L30x <macdizofti032@gmail.com>
2021-02-10 21:18:16 +0700

    Variable color

---
+++ wifck.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# wifck
+# https://github.com/xG4L1L30x/wifck
+# Author: G4L1L30
+# Version: -
+
+H='\033[30m'
+R='\033[31m'
+G='\033[32m'
+B='\033[34m'
+P='\033[35m'
+C='\033[36m'
+Y='\033[33m'
+W='\033[97m'
+bgH='\033[40m'
+bgR='\033[41m'
+bgG='\033[42m'
+bgB='\033[44m'
+bgP='\033[45m'
+bgC='\033[46m'
+bgY='\033[43m'
+bgW='\033[107m'
+BD='\033[1m'
+Q='\033[0m'
